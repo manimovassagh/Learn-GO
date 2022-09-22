a900899fcee4c4af88048e0d7dcce06269eb95e2
Mani Movassagh <mani.mgh@gmail.com>
2022-09-22 15:13:20 +0200

    variables

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "app/variables"
+
+func main() {
+	variables.Variables()
+}
---
+++ variables/variables.go
@@ -0,0 +1,14 @@
+package variables
+
+import "fmt"
+
+func Variables() {
+
+	var x int
+	var y int
+	var z, m int
+	z = 2
+	m = 3
+	fmt.Println(x, y)
+	fmt.Println(z, m)
+}
