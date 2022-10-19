052955992869f0fbf3dadd1fe6e0db062e62e9f7
Mani Movassagh <mani.mgh@gmail.com>
2022-10-19 14:33:17 +0200

    n

--- variables/escape-sequence/escape-sequence.go
+++ variables/escape-sequence/escape-sequence.go
@@ -2,6 +2,13 @@ package escapesequence
 
 import "fmt"
 
+var (
+	customer = "Mani Movassagh"
+	c        = "sahar"
+)
+
 func EscapeSequence() {
 	fmt.Println("Hi Mani\nthis is new Line\nand this is another line")
+	fmt.Printf("Hi: %v", customer)
+	fmt.Printf("\nHi: %v", c)
 }
