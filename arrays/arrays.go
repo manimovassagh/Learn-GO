2871622541fd8eb2c09ab6b3da9f4358c613720b
Mani Movassagh <mani.mgh@gmail.com>
2022-10-20 23:58:35 +0200

    slice

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,15 +1,21 @@
 package arrays
 
-import "log"
+import (
+	"fmt"
+)
 
 func PrintArray() {
-	arr := [4]string{"mani", "sahar", "some", "mehdi"}
-	t := arr[3]
-	l := arr[2]
-	a := arr[1]
-	log.Println(arr)
-	log.Println(t)
-	log.Println(l)
-	log.Println(a)
-	log.Println(len(arr))
+	// arr := [4]string{"mani", "sahar", "some", "mehdi"}
+	// t := arr[3]
+	// l := arr[2]
+	// a := arr[1]
+	// log.Println(arr)
+	// log.Println(t)
+	// log.Println(l)
+	// log.Println(a)
+	// log.Println(len(arr))
+	var sliceSample = []string{"sahar"}
+	sliceSample = append(sliceSample, "mani")
+	fmt.Println(sliceSample)
+
 }
